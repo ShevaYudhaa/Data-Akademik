fcd4572adcfbc841e04bf9e52d3a8cc9ec4aa4f7
bropien <mzakkyraihan@gmail>
2025-10-17 09:14:20 +0700

    1 JAM JAGO FORSAKEN

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div className="container text-center mt-5">
         <h1 className="text-primary fw-bold">Sistem Data Akademik</h1>
-        <p className="mt-3">Selamat datang di aplikasi manajemen master data akademik SIA.</p>
+        <p className="mt-3">Selamat datang di aplikasi manajemen master data akademik SIA FORSAKEN.</p>
         </div>
     );
     }
